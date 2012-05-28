4f0597aa88d5bb763090ee38baffb098e9c4b37a
unknown <MH@MH-PC.(none)>
2012-05-28 17:50:09 +0300

    tot

--- Tsohantyo/src/main/java/harkka/tsohantyo/Kontrolleri.java
+++ Tsohantyo/src/main/java/harkka/tsohantyo/Kontrolleri.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class Kontrolleri {
 
     @RequestMapping("testi")
-    public String heiSpring() {
+    public String heijaSpring() {
         System.out.println("Heippa!");
         return "index.jsp";
     }
